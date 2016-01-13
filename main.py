4cf674fe9737ce89595a499f9d7a123d18c2529c
Saurabh Kumar <theskumar@users.noreply.github.com>
2016-01-13 23:50:47 +0530

    Make things heroku compatible

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
-from flask import Flask, Response
 from os import environ
-from json import dumps
+
 import requests
+from flask import Flask
 from flask.ext.cors import CORS
+from flask.json import jsonify
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 # You can get API_KEY from here: https://www.explara.com/a/account/manage/app-settings
-# Your API_KEY = 'Bearer <YourKeyHere>'
+# Your API_KEY = '<YourKeyHere>'
 API_KEY = environ['API_KEY']
 TICKETS_URL = 'https://www.explara.com/api/e/get-tickets'
 # EVENT_ID you can get via this request: https://www.explara.com/api/e/get-all-events
 EVENT_ID = environ['EVENT_ID']
 
+
 @app.route('/api/tickets-left/')
 def get_remaining_tickets_count():
-    headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': API_KEY}
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': "Bearer %s" % API_KEY}
     payload = {'eventId': EVENT_ID}
     response = requests.post(TICKETS_URL, data=payload, headers=headers)
-    return Response(dumps({"tickets_left": response.json()["tickets"][0]["quantity"]}),
-                                status=200,
-                                content_type="application/json")
+    data = response.json()
+    try:
+        data_response = {
+            'tickets_left': data["tickets"][0]["quantity"]
+        }
+    except KeyError:
+        return jsonify(**data)
+    return jsonify(**data_response)
 
-app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
